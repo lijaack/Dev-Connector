6ad0a12b031d42fdd6f952ddd6e32ce2b6a212e9
Jack <lijaack@yahoo.com>
2019-03-27 23:21:49 -0700

    added text field component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -22,7 +22,7 @@ class Login extends Component {
       this.props.history.push('/dashboard');
     }
   }
-
+  
   componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
       this.props.history.push('/dashboard');
