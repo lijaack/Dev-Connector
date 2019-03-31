8365f2e05a8ef9bdec9b5e8d379b7126af59fb9f
Jack <lijaack@yahoo.com>
2019-03-31 01:32:19 -0700

    update keys

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://jack:1m3ZcylV88n5BOc1@cluster0-yra9z.mongodb.net/devconnector?retryWrites=true',
+    mongoURI: 'mongodb+srv://jack:1m3ZcylV88n5BOc1@cluster0-yra9z.mongodb.net/devconnector',
     secretOrKey: 'secret'
 };
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI,
+    mongoURI: "mongodb+srv://jack:1m3ZcylV88n5BOc1@cluster0-yra9z.mongodb.net/devconnector",
     secretOrKey: process.env.SECRET_OR_KEY
 };
